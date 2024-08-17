e53d9906dc586d724e38505671fca9779c7c139d
RUPAM GIRI <rupamgiri4321@gmail.com>
2024-08-17 22:21:38 +0530

    file modify & LinkedList

---
+++ Matrix-DSA/6-menu-driven-program.cpp
@@ -0,0 +1,39 @@
+
+
+// void main()
+// {
+//     int *A, n, ch, x;
+//     printf("Enter Dimention:");
+//     scanf("%d", &n);
+
+//     A = (int *)malloc(n * sizeof(int));
+
+//     do
+//     {
+//         // display menu
+//         switch (ch)
+//         {
+//         case 1:
+//             for (int i = 0; I <= n; i++)
+//             {
+//                 scanf("%d", &A[i - 1]);
+//                 break;
+//             }
+//         case 2:
+//             printf("Enter Index:");
+//             scanf("%d %d", &i, &j);
+//             if (i == j)
+//                 printf("%d", A[i - 1]);
+//             else
+//                 printf("0");
+//         }
+//     case 3:
+//         printf("Enter roe,col and ele:");
+//         scanf("%d %d %d", &i, &j, &x);
+//         if (i == j)
+//             A[i - 1] = x;
+//         break;
+//     } while ()
+
+//         return 0;
+// }
